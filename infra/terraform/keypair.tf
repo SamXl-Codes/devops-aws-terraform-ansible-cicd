f97283aa337a3a779e534ad05a247112f5d7a2a0
SamXI-Codes <sammielusie.cr7@gmail.com>
2025-11-06 23:31:26 +0000

    Fix keypair.tf to use base64 public key secret

--- infra/terraform/keypair.tf
+++ infra/terraform/keypair.tf
@@ -1,4 +1,13 @@
+variable "public_key_base64" {
+  description = "Base64-encoded public key for EC2 key pair"
+  type        = string
+}
+
 resource "aws_key_pair" "main" {
   key_name   = "ca-ssh-key"
-  public_key = file(var.public_key_path)
+  public_key = base64decode(var.public_key_base64)
+
+  lifecycle {
+    ignore_changes = [public_key]
+  }
 }
